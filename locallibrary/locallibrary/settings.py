68b14eb783d08fac83c42919de02096aa6e860c5
fym666 <47251510+fym666@users.noreply.github.com>
2020-11-28 15:13:39 +0800

    Update settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -86,23 +86,14 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.postgresql',
- 'NAME': 'hotzone2',
- 'USER': 'stephen',
- 'PASSWORD': 'Wsq809327698',
- 'HOST': 'localhost',
- 'PORT': '',
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
 
 # DATABASES = {
 #  'default': env.dj_db_url('DATABASE_URL')

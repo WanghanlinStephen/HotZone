93fae4569955d6495af28b9193d43fa05bf59b20
Stephen <u3560396@connect.hku.hk>
2020-11-29 15:10:51 +0800

    Deployment Sprint3

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -86,18 +86,28 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
-#  'default': env.dj_db_url('DATABASE_URL')
+# 'default': {
+#  'ENGINE': 'django.db.backends.postgresql',
+#  'NAME': 'hotzone2',
+#  'USER': 'stephen',
+#  'PASSWORD': 'Wsq809327698',
+#  'HOST': 'localhost',
+#  'PORT': '',
+#  }
 # }
+
+
+DATABASES = {
+ 'default': env.dj_db_url('DATABASE_URL')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
